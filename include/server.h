85585d27b0d8708c8d4255273037722a11b3e723
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-07 23:31:51 +0200

    Move library include directives into a server header file

--- include/server.h
+++ include/server.h
@@ -6,10 +6,15 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 21:11:45 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/07 21:12:20 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/07 23:28:45 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "server.h"
+# include "libft.h"
 #endif
--- src/server.c
+++ src/server.c
@@ -6,22 +6,21 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:16:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/07 22:25:23 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/07 23:29:07 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "server.h"
-#include "libft.h"
 
 static void	signal_handler(int signum);
 
 int	main(void)
 {
+	struct sigaction action;
+
+	
 	ft_printf("Server PID: %d\n", getpid());
-	signal(SIGTERM, signal_handler);
+	
 	while (1)
 		pause();
 	return (0);
