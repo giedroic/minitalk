7c44918339c82d012dbfdfc22917aec2d86e447d
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 15:53:41 +0200

    Rename sigaction structure variable

--- src/utils.c
+++ src/utils.c
@@ -6,11 +6,19 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:05:05 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 15:05:13 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 15:53:05 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	Signal(int sig, void *signal_handler, int use_siginfo)
+void	Signal(int sig, void *handler, int use_siginfo)
 {
-	;
+	struct sigaction	act;
+
+	if (use_siginfo)
+	{
+		act.sa_flags = SA_SIGINFO;
+		act.sa_sigaction = signal_handler;
+	}
+	else
+		act.sa_handler = signal_handler;
 }
