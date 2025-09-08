d335ab1b85493750308cc244bb8d887b1a21f1c5
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 16:25:36 +0200

    Change variable name

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:05:05 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 16:24:18 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 16:25:22 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	signal_wrapper(int signum, void *handler, int use_siginfo)
 		act.sa_sigaction = handler;
 	}
 	else
-		act.sa_handler = signal_handler;
+		act.sa_handler = handler;
 	sigemptyset(&act.sa_mask);
 	sigaddset(&act.sa_mask, SIGUSR1);
 	sigaddset(&act.sa_mask, SIGUSR2);
