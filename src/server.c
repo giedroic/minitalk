c43be5bcf53e5a1d465979fedeea24ff547254db
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 20:50:22 +0200

    Change argument of kill_wraper function

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:16:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 20:37:43 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 20:50:05 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,11 @@ void	handler(int signum, siginfo_t *info, void *ucontext)
 		if (byte == '\0')
 		{
 			write(STDOUT_FILENO, "\n", 1);
-			kill_wraper(info->si_sipid, SIGUSR2);
+			kill_wraper(info->si_pid, SIGUSR2);
 			return ;
 		}
 		byte = 0;
 		write(STDOUT_FILENO, &byte, 1);
 	}
-	kill_wraper(info->si_sipid, SIGUSR1);
+	kill_wraper(info->si_pid, SIGUSR1);
 }
