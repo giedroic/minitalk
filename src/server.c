0e39ee5e9711d50a0f9c822d4dc5bdef6de2dd17
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 14:42:35 +0200

    Remove signal_handler function

--- src/server.c
+++ src/server.c
@@ -6,26 +6,16 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:16:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 13:20:14 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 14:42:17 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	signal_handler(int signum);
-
 int	main(void)
 {
-	
 	ft_printf("Server PID: %d\n", getpid());
-	
 	while (1)
 		pause();
-	return (0);
-}
-
-static void	signal_handler(int signum)
-{
-	ft_printf("SIGTERM: %d\n", signum);
-	exit(0);
+	return (EXIT_SUCCESS);
 }
