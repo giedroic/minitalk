8c668714934851883c6c64dfc35e24f88c68c42b
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 15:06:07 +0200

    Add Signal function wrapper

--- src/utils.c
+++ src/utils.c
@@ -1,21 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/07 21:11:45 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 15:04:53 by agiedroi         ###   ########.fr       */
+/*   Created: 2025/09/08 15:05:05 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/09/08 15:05:13 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_H
-# define SERVER_H
-# include <signal.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include "libft.h"
-
-void	Signal(int sig, void *signal_handler, int use_siginfo);
-#endif
+void	Signal(int sig, void *signal_handler, int use_siginfo)
+{
+	;
+}
