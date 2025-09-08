a2de2e8845431d0680366e2a7ac5315f455f32c0
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 16:35:21 +0200

    Change handler function variable name

--- src/server.c
+++ src/server.c
@@ -6,13 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:16:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 16:06:35 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 16:35:03 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handler(int sig, siginfo_t *info, void *ucontext);
+void	handler(int signum, siginfo_t *info, void *ucontext);
 
 int	main(void)
 {
@@ -22,7 +22,7 @@ int	main(void)
 	return (EXIT_SUCCESS);
 }
 
-void	handler(int sig, siginfo_t *info, void *ucontext)
+void	handler(int signum, siginfo_t *info, void *ucontext)
 {
 	(void) ucontext;
 }
