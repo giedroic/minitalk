d9c49ca4778d769653c9af0ba4a21f49ca473751
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 16:21:09 +0200

    Call and check for errors sigaction function

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:05:05 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 16:15:22 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 16:20:32 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,9 @@ void	signal_wrapper(int signum, void *handler, int use_siginfo)
 	sigemptyset(&act.sa_mask);
 	sigaddset(&act.sa_mask, SIGUSR1);
 	sigaddset(&act.sa_mask, SIGUSR2);
+	if (sigaction(signum, &act, NULL) < 0)
+	{
+		ft_printf("Sigaction failed");
+		exit(EXIT_FAILURE);
+	}
 }
