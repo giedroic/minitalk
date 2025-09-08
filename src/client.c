f0d90bc6ed7913d4cf475bf40f15f645ee6656d8
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 20:39:06 +0200

    Implement end_handler function

--- src/client.c
+++ src/client.c
@@ -6,13 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 13:21:20 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 20:27:56 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 20:37:01 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile sig_atomic_t g_server;
+volatile sig_atomic_t	g_server;
 void	ack_handler(int signum);
 void	end_handler(int signum);
 void	send_byte(char byte, pid_t pid);
@@ -39,12 +39,13 @@ int	main(int argc, char *argv[])
 
 void	ack_handler(int signum)
 {
-	g_server = READY;	
-}	
+	g_server = READY;
+}
 
 void	end_handler(int signum)
 {
-
+	write(STDOUT_FILENO, "String received!\n", 17);
+	exit (EXIT_SUCCESS);
 }
 
 void	send_byte(char byte, pid_t pid)
