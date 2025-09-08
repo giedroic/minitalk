4d77539811e3242ca0dfda41a2e27af0f3c5ab1d
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 16:59:45 +0200

    Add necessary variables and check for number of arguments

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 13:21:20 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 13:21:56 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 16:59:09 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	main(int argc, char *argv[])
 {
+	pid_t	server;
+	char	*string;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client <server_PID> <string>\n");
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
