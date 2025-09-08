14e618d1b2de1e8d93ef091afbb72a3258a95f36
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 19:38:40 +0200

    Do enum for two flag states BUSY READY

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 21:11:45 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 18:20:06 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 19:37:40 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 # include <unistd.h>
 # include "libft.h"
 
+enum e_state
+{
+	BUSY,
+	READY,
+};
 void	kill_wraper(pid_t pid, int sig);
 void	signal_wrapper(int signum, void *signal_handler, int use_siginfo);
 #endif
