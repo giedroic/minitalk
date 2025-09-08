47faf54072d25becc34c6e5a5a80bd56e608d3e2
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 18:20:47 +0200

    Change header protection name

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,18 +6,18 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 21:11:45 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 17:55:27 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 18:20:06 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_H
-# define SERVER_H
+#ifndef MINITALK_H
+# define MINITALK_H
 # include <limits.h>
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
 
-void	kill_wraper(pid_t pid, int sig)
+void	kill_wraper(pid_t pid, int sig);
 void	signal_wrapper(int signum, void *signal_handler, int use_siginfo);
 #endif
