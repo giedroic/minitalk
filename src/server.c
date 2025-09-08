4552698f8c9cf41e798ec2955b0bd0773a7acf8a
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-08 15:00:23 +0200

    Add signal_handler funciton

--- src/server.c
+++ src/server.c
@@ -6,11 +6,13 @@
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 20:16:04 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/08 14:42:17 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/08 14:59:52 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	signal_handler(int sig, siginfo_t *info, void *more_info);
 
 int	main(void)
 {
@@ -19,3 +21,8 @@ int	main(void)
 		pause();
 	return (EXIT_SUCCESS);
 }
+
+void	signal_handler(int sig, siginfo_t *info, void *more_info)
+{
+	(void) more_info;
+}	
