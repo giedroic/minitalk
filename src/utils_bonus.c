2e6a1c143fc8759258f006b2addebfe3de5aed53
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-10 12:50:59 +0200

    Add bonus utilities source file

---
+++ src/utils_bonus.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 15:05:05 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/09/08 17:51:17 by agiedroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	kill_wraper(pid_t pid, int sig)
+{
+	if (kill(pid, sig) < 0)
+	{
+		write(STDERR_FILENO, "kill error\n", 11);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	signal_wrapper(int signum, void *handler, int use_siginfo)
+{
+	struct sigaction	act;
+
+	ft_bzero(&act, sizeof(act));
+	if (use_siginfo)
+	{
+		act.sa_flags = SA_SIGINFO;
+		act.sa_sigaction = handler;
+	}
+	else
+		act.sa_handler = handler;
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, SIGUSR1);
+	sigaddset(&act.sa_mask, SIGUSR2);
+	if (sigaction(signum, &act, NULL) < 0)
+	{
+		write(STDERR_FILENO, "sigaction error\n", 16);
+		exit(EXIT_FAILURE);
+	}
+}
